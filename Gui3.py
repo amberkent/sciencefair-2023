711c63d46bb675e5485394f7730497f368f99468
amberkent <122503253+amberkent@users.noreply.github.com>
2023-01-14 14:23:10 -0700

    attempt 1 at deleting window around image

---
+++ Gui3.py
@@ -0,0 +1,33 @@
+import io
+import os
+import PySimpleGUI as sg
+from PIL import Image
+
+def main():
+    layout = [
+        [sg.Image(key="-IMAGE-")],
+
+    ]
+    window = sg.Window("Image Viewer", layout, element_padding=(0,0))
+    window.finalize()
+    
+    filename = "Dog.jpg"
+    
+    if os.path.exists(filename):
+        image = Image.open(filename)
+        image.thumbnail((400, 400))
+        bio = io.BytesIO()
+        image.save(bio, format="PNG")
+        window["-IMAGE-"].update(data=bio.getvalue())
+    else:
+      raise Error("ERROR: NO FILE")
+        
+    
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+if __name__ == "__main__":
+    main()
