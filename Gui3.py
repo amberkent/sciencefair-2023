7fb5e295e7f3474ce3a16dbb8cfdd9ae8a1f62af
amberkent <122503253+amberkent@users.noreply.github.com>
2023-02-02 20:38:40 -0700

    Changing image

--- Gui3.py
+++ Gui3.py
@@ -11,7 +11,7 @@ def main():
     window = sg.Window("Image Viewer", layout,)
     window.finalize()
     
-    filename = "images/vampire.jpeg"
+    filename = "images/scuba.jpeg"
     
     if os.path.exists(filename):
         image = Image.open(filename)
