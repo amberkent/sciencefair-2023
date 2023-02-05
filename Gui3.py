eca1cd70bfda0f3a610c22f4d4c0eda783939d57
amberkent <122503253+amberkent@users.noreply.github.com>
2023-02-05 15:43:56 -0700

    perfect()
    def perfect():
        layout = [
            [sg.Image(key="-IMAGE-", background_color='black', pad=(0, 0))],
    
        ]
        window = sg.Window("Image Viewer", layout,)
        window.finalize()
        
        filename = "images/perfect.jpeg"
        
        if os.path.exists(filename):
            image = Image.open(filename)
            image.thumbnail((400, 400))
            bio = io.BytesIO()
            image.save(bio, format="PNG")
            window["-IMAGE-"].update(data=bio.getvalue())
        else:
          raise Error("ERROR: NO FILE")
            
        
        while True:
            event, values = window.read()
            if event == "Exit" or event == sg.WIN_CLOSED:
                break
    
        window.close()
    if __name__ == "__main__":
        time.sleep(3)
        
        perfect()

--- Gui3.py
+++ Gui3.py
@@ -1,9 +1,10 @@
 import io
 import os
+import time
 import PySimpleGUI as sg
 from PIL import Image
 
-def main():
+def scuba():
     layout = [
         [sg.Image(key="-IMAGE-", background_color='black', pad=(0, 0))],
 
@@ -30,4 +31,36 @@ def main():
 
     window.close()
 if __name__ == "__main__":
-    main()
+    scuba()
+    time.sleep(3)
+    
+def perfect():
+    layout = [
+        [sg.Image(key="-IMAGE-", background_color='black', pad=(0, 0))],
+
+    ]
+    window = sg.Window("Image Viewer", layout,)
+    window.finalize()
+    
+    filename = "images/perfect.jpeg"
+    
+    if os.path.exists(filename):
+        image = Image.open(filename)
+        image.thumbnail((400, 400))
+        bio = io.BytesIO()
+        image.save(bio, format="PNG")
+        window["-IMAGE-"].update(data=bio.getvalue())
+    else:
+      raise Error("ERROR: NO FILE")
+        
+    
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+if __name__ == "__main__":
+    time.sleep(3)
+    
+    perfect()
