e8827f2281f7a82991dd79cfc40bcad462652009
amberkent <122503253+amberkent@users.noreply.github.com>
2023-03-18 16:06:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def set_image(filename):
     if os.path.exists(filename):
         image = Image.open(filename)
         image.thumbnail((400, 400))
-        image = image.resize((1000,400))
+        image = image.resize((900,400))
         bio = io.BytesIO()
         image.save(bio, format="PNG")
         window["-IMAGE-"].update(data=bio.getvalue())
